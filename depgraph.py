7245e4c29c3d43f20929c7b18965830d0b60f546
sugaken <sugaken.r3@gmail.com>
2011-09-07 11:28:58 +0900

    changed subdirectory name

--- depgraph.py
+++ depgraph.py
@@ -3,7 +3,7 @@ import rpm
 import sys
 from optparse import OptionParser
 import operator
-from depgraph.mylist import flatten,uniq
+from dgsub.mylist import flatten,uniq
 
 Feat = {} # Feat[feature] => [ providers ]
 File = {} # File[filename] => [ providers ]
